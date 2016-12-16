112569d07a8eec3860c1525ebb21b9e7b5673e3e
Sebastian Hodapp <github@sebastian-hodapp.de>
2016-12-16 08:40:41 +0100

    Environment Variables set

--- server.js
+++ server.js
@@ -20,8 +20,8 @@ var MyRed = function () {
      */
     self.setupVariables = function () {
         //  Set the environment variables we need.
-        self.ipaddress = env.OPENSHIFT_NODEJS_IP;
-        self.port = env.OPENSHIFT_NODEJS_PORT || 8000;
+        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+        self.port = process.env.OPENSHIFT_NODEJS_PORT || 8000;
 
         if (typeof self.ipaddress === "undefined") {
             //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
